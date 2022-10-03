8a1c19ed932ec9af605c79ae047491f3d56ecf30
Marcin Skiper <jaykubiak0201@gmail.com>
2022-10-03 20:37:15 +0200

    Task 1 not aligned

--- playground.dart
+++ playground.dart
@@ -1,3 +1,14 @@
 main() {
-  print("Oto szesciany wybranych liczb");
+  int k1 = 21;
+  int k2 = 32;
+  int k3 = -53;
+
+  print('Oto szesciany wybranych liczb \n');
+  print('k        k*k*k \n');
+  print(k1);
+  print(k1*k1*k1);
+  print(k2);
+  print(k2*k2*k2);
+  print(k3);
+  print(k3*k3*k3);
 }
