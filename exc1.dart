cd51974f511a729c7720f65b14ef2bc4d861c836
Marcin <84280969+nemoforte@users.noreply.github.com>
2022-10-10 20:35:34 +0200

    Update exc1.dart
    so I WILL NOT BREAK ANY SOLID RULES

--- exc1.dart
+++ exc1.dart
@@ -1,48 +1,51 @@
+/*Napisz program, który wypisze poniższy tekst.
+Do obliczenia sześcianów użyj operacji mnożenia.
+Oto szesciany wybranych liczb:
+k k*k*k
+21 9261
+32 32768
+-53 -148877
+Koniec programu.*/
+
+import 'dart:io';
+
 main() {
-
-  int k1 = 21;
-  int k2 = 32;
-  int k3 = -53;
-
-  int k31 = k1*k1*k1;
-  int k32 = k2*k2*k2;
-  int k33 = k3*k3*k3;
-
-  int len0 = 'k'.length;           //1
-  int len1 = k1.toString().length; //2
-  int len2 = k2.toString().length; //2
-  int len3 = k3.toString().length; //3
-
-  String gap0 = "";
+  List list = ['k', 'k*k*k'];
+  String? nxt;
+  int defgap1 = 0;
   String gap1 = "";
-  String gap2 = "";
-  String gap3 = "";
-  int defgaplen = 10;
-  
-  len0 = defgaplen - len0; //9
-  len1 = defgaplen - len1; //8
-  len2 = defgaplen - len2; //8
-  len3 = defgaplen - len3; //7
 
-  for (int i = 1; i <= len0; ++i) {
-  gap0 = gap0 + " ";
+  stdout.writeln('input number');
+  nxt = stdin.readLineSync();
+
+  while (nxt != 'stop') {
+    if (nxt != null) {
+      list.add(nxt);
+      int k = int.parse(nxt);
+      k = k*k*k;
+      nxt = k.toString();
+      list.add(nxt);
+      stdout.writeln("input another number or 'stop'");
+      nxt = stdin.readLineSync();
+    }
   }
 
-  for (int i = 1; i <= len1; ++i) {
-  gap1 = gap1 + " ";
+  for (int i = 0; i < list.length; i=i+2) {
+    if (defgap1 < list[i].length) {
+      defgap1 = list[i].length;
+    }
   }
 
-  for (int i = 1; i <= len2; ++i) {
-  gap2 = gap2 + " ";
+  print('Oto szesciany wybranych liczb: \n');
+
+  for (int i = 0; i < list.length; i=i+2) {
+    for (int j = 0; j <= (3*defgap1)-list[i].length; j++) {
+      gap1 = gap1 + " ";
+    }
+  print(list[i]+gap1+list[i+1]);
+  gap1 = "";
   }
 
-  for (int i = 1; i <= len3; ++i) {
-  gap3 = gap3 + " ";
-  }
+  print('\nKoniec programu');
 
-  print('Oto szesciany wybranych liczb \n');
-  print('k'+'$gap0'+'k*k*k \n');
-  print('$k1' + '$gap1' + '$k31');
-  print('$k2' + '$gap2' + '$k32');
-  print('$k3' + '$gap3' + '$k33');
 }
