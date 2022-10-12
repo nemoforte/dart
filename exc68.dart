e20ab3cbde7901dc72ca1754b7a94ee79f8e216c
Marcin <84280969+nemoforte@users.noreply.github.com>
2022-10-12 19:30:48 +0200

    Create exc68.dart

---
+++ exc68.dart
@@ -0,0 +1,28 @@
+/*Napisz program, który rysuje na ekranie
+gwiazdki ułożone w „kwadrat”.
+***
+***
+***
+Liczbę gwiazdek w jednym rzędzie podaje użytkownik.*/
+
+import 'dart:io';
+
+main() {
+
+  String row = '';
+  stdout.writeln('podaj liczbe gwiazdek w jednym rzedzie');
+  String? g = stdin.readLineSync();
+  if (g != null){
+    int k = int.parse(g);
+    print(k);
+
+    for (int i = 1; i <= k; i++) {
+      row = row + '*';
+    }
+    
+    for (int i=1; i <= k; i++) {
+      print(row);
+    }
+
+  }
+}
